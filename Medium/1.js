4a30978ee9eb325778ebfe558e7dd51e3876b0d7
Sirius Black <mariia.beliaev@gmail.com>
2023-03-01 00:38:35 +0300

    added task 1 medium

--- Medium/1.js
+++ Medium/1.js
@@ -5,8 +5,10 @@
 */
 
 const logEachLetter = (str) => {
-    /* Тут код*/
+  let arr = str.split("");
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
 
-}
-
-module.exports = logEachLetter
+module.exports = logEachLetter;

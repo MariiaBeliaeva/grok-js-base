39c8082b0311da1a6f78f6609de4465891f9883a
Sirius Black <mariia.beliaev@gmail.com>
2023-03-14 00:14:46 +0300

    added task medium 3

--- Medium/3.js
+++ Medium/3.js
@@ -1,17 +1,24 @@
 /*
-
 Создайте функцию, которая принимает в качестве аргументов 2 неотрицательных числа, 
 а возвращает сумму всех целых чисел в диапазоне между ними.
-
 Первое число может больше второго!
-
 Пример: (5, 3) => 12
-
 */
 
 const sumOfIntegers = (start, end) => {
-    /* Тут код*/
+  let sum = 0;
+  if (start < end) {
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+  } else if (start > end) {
+    for (let i = end; i <= start; i++) {
+      sum += i;
+    }
+  } else if ((start = end)) {
+    sum = start;
+  }
+  return sum;
+};
 
-}
-
-module.exports = sumOfIntegers
+module.exports = sumOfIntegers;
